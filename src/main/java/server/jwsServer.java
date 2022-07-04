a16981dfea270fa59fc788bb9dd139e7debc907f
thediaf <jaharacs98@gmail.com>
2022-07-04 11:39:14 +0000

    last update ;)

--- src/main/java/server/jwsServer.java
+++ src/main/java/server/jwsServer.java
@@ -7,7 +7,7 @@ import service.BankService;
 public class jwsServer {
 
     public static void main(String[] args) {
-        String url = "http://localhost:8020/";
+        String url = "http://192.168.5.51:8020/";
 
         Endpoint.publish(url, new BankService());
         System.out.println(url);
